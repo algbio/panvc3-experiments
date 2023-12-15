8d02d9c049ff63121d4e6ae3e02006d5aa7bee33
Tuukka Norri <tsnorri@iki.fi>
2023-12-15 23:39:46 +0200

    Add vcf2multialign’s memory requirement to command line parameters
    – So far I haven’t been able to make Snakemake accept a workflow profile, so this seemed to be the only option.

--- take-one-out-chr1/run-snakemake-test.sh
+++ take-one-out-chr1/run-snakemake-test.sh
@@ -2,4 +2,4 @@
 
 set -euxo pipefail
 
-snakemake --printshellcmds --use-conda --conda-prefix ../conda-env --cores 40 --rerun-triggers mtime --dry-run -- all
+snakemake --printshellcmds --use-conda --conda-prefix ../conda-env --cores 40 --rerun-triggers mtime --default-resources mem_mb=172032 --set-resources panvc3_generate_founder_sequences:mem_mb=81920 --dry-run -- all
