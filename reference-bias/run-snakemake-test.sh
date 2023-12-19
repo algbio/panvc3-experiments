89cdbdc4e7b1268963af748faa57af17a7fcb326
Tuukka Norri <tsnorri@iki.fi>
2023-12-19 11:46:17 +0200

    Consider only modification time for re-running

--- reference-bias/run-snakemake-test.sh
+++ reference-bias/run-snakemake-test.sh
@@ -2,4 +2,4 @@
 
 set -euxo pipefail
 
-snakemake --printshellcmds --use-conda --conda-prefix ../conda-env --cores 40 --dry-run -- all
+snakemake --printshellcmds --use-conda --conda-prefix ../conda-env --cores 40 --rerun-triggers mtime --dry-run -- all
