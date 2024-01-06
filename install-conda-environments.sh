bdfb54121a441357693d81d66f1a40b2ff3a1c64
Tuukka Norri <tsnorri@iki.fi>
2024-01-06 20:17:06 +0200

    Update Conda environment paths

--- install-conda-environments.sh
+++ install-conda-environments.sh
@@ -2,7 +2,7 @@
 
 set -euxo pipefail
 
-for target in bcftools biopython bowtie2 bwa gatk graphtyper2 gridss happy manta mason samtools truvari vg
+for target in bcftools biopython bowtie2 bwa gatk graphtyper2 gridss happy manta mason panvc3 samtools truvari vcf2multialign vg
 do
 	snakemake -c 1 --use-conda --conda-prefix conda-env --snakefile workflow/rules/install.smk -- ${target}
 done
